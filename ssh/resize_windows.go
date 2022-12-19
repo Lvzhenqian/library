0c788249c510a5e813866c06a61d49608f3295fa
lvzhenqian <276829290@qq.com>
2022-12-19 11:14:55 +0800

    ssh库修改实现

--- ssh/resize_windows.go
+++ ssh/resize_windows.go
@@ -2,7 +2,27 @@
 
 package ssh
 
+import (
+	terminal "golang.org/x/term"
+	"time"
+)
+
 // windows not support Terminal Resize
-func (c *clientType) updateTerminalSize() {
-	return
+func (c *ClientType) updateTerminalSize(fd, termWidth, termHeight int, failed chan error) {
+	for range time.Tick(time.Second) {
+		currTermWidth, currTermHeight, getSizeErr := terminal.GetSize(fd)
+		if getSizeErr != nil {
+			failed <- getSizeErr
+			continue
+		}
+		if currTermHeight == termHeight && currTermWidth == termWidth {
+			continue
+		}
+
+		if changeErr := c.session.WindowChange(currTermHeight, currTermWidth); changeErr != nil {
+			failed <- changeErr
+			continue
+		}
+		termWidth, termHeight = currTermWidth, currTermHeight
+	}
 }
